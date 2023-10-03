af3cd07f708675715295509ef675bcf24f274335
Dylan DeBord <91545068+dylandebord729@users.noreply.github.com>
2023-10-02 22:55:41 -0400

    Add files via upload

---
+++ DeBord_lab5.py
@@ -0,0 +1,124 @@
+#Problem13
+
+#Addition
+print(4+3+2)
+print(4.1+3.1+2.1)
+print("4"+"3"+"2")
+
+#Subtract
+print(4-3-2)
+print(4.1-3.1-2.1)
+print("4"-"3"-"2")
+
+#Multiplication
+print(4*3*2)
+print(4.1*3.1*2.1)
+print("4"*"3"*"2")
+
+#Exponental
+print(4**3)
+print(4.1**3.1)
+print("4"**"3")
+
+#Divison
+print(4/3)
+print(4.1/3.1)
+print("4"/"3")
+
+#Mod
+print(4%3)
+print(4.1%3.1)
+print("4"%"3")
+
+#<<
+print(4<<3)
+print(4.1<<3)
+print("4"<<"3")
+
+#>>
+print(4>>3)
+print(4.1>>3.1)
+print("4">>"3")
+
+#&
+print(4&3)
+print(4.1&3.1)
+print("4"&"3")
+
+#|
+print(4|3)
+print(4.1|3.1)
+print("4"|"3")
+
+#^
+print(4^3)
+print(4.1^3.1)
+print("4"^"3")
+
+#<
+print(4<3)
+print(4.1<3.1)
+print("4"<"3")
+
+#>
+print(4>3)
+print(4.1>3.1)
+print("4">"3")
+
+#<=
+print(4<=3)
+print(4.1<=3.1)
+print("4"<="3")
+
+#>=
+print(4>=3)
+print(4.1>=3.1)
+print("4">="3")
+
+#==
+print(4==3)
+print(4.1==3.1)
+print("4"=="3")
+
+#!=
+print(4!=3)
+print(4.1!=3.1)
+print("4"!="3")
+
+#<>
+print(4<>3)
+print(4.1<>3.1)
+print("4"<>"3")
+
+
+#Problem20
+#Tokyo
+w = "kyoto"
+Final = w[3:]+w[:3]
+print (Final)
+
+#Bead
+w = "Bread"
+Final = w[0]+w[2:]
+print (Final)
+
+#Surakarta
+w = "kartasura"
+Final = w[5:]+w[:5]
+print (Final)
+
+#Silent
+w = "listen"
+Final = w[2]+w[1]+w[0]+w[4:]+w[3]
+print (Final)
+
+
+#Problem29
+import random
+r = random.random()
+print (r)
+
+a = 3.2
+b = 6.4
+r = random.randint(a,b)
+print (r)
